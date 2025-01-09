fd3ccca5ccf8ebade35414ab02eb7fdd0e72de15
Prithvi Gottumukkala <prithviraju@Prithvis-MacBook-Pro.local>
2025-01-09 18:17:34 +0530

    first push

---
+++ src/components/navBar/NavBar.jsx
@@ -0,0 +1,12 @@
+
+
+export default function NavBar (){
+
+    return <nav>
+        <ul>
+            <li>Resume</li>
+            <li>Project</li>
+            <li>Contact</li>
+        </ul>
+    </nav>
+}
